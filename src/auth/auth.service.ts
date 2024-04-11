891114122d078e9340ef973f7e2bc9b1d27b3dbb
Maksym Tkachuk <maksym.tkachuk@infopulse.com>
2024-04-11 12:36:50 +0300

    update(auth): add refresh token logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,11 +6,15 @@ import {
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { verify } from 'argon2';
+import { Response } from 'express';
 import { UserService } from 'src/user/user.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
+  EXPIRE_DAY_REFRESH_TOKEN = 1;
+  REFRESH_TOKEN_NAME = 'refreshToken';
+
   constructor(
     private jwt: JwtService,
     private userService: UserService,
@@ -66,5 +70,45 @@ export class AuthService {
     if (!isValid) throw new UnauthorizedException('Invalid password or email');
 
     return user;
+  }
+
+  addRefreshTokenToResponse(res: Response, refreshToken: string) {
+    const expiresIn = new Date();
+    expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN);
+
+    res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: expiresIn,
+      secure: true,
+      // lax if production
+      sameSite: 'none',
+    });
+  }
+
+  removeRefreshTokenFromResponse(res: Response) {
+    res.cookie(this.REFRESH_TOKEN_NAME, '', {
+      httpOnly: true,
+      domain: 'localhost',
+      expires: new Date(0),
+      secure: true,
+      // lax if production
+      sameSite: 'none',
+    });
+  }
+
+  async getNewTokens(refreshToken: string) {
+    const result = await this.jwt.verifyAsync(refreshToken);
+    if (!result) throw new UnauthorizedException('Invalid refresh token');
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...user } = await this.userService.getById(result.id);
+
+    const tokens = this.createTokens(user.id);
+
+    return {
+      user,
+      ...tokens,
+    };
   }
 }
