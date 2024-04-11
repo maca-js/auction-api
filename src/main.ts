19d086a890bffcb4cd000747649d73f0ce7fb748
Maksym Tkachuk <maksym.tkachuk@infopulse.com>
2024-04-11 10:11:58 +0300

    update(main.ts): add basic settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
+  await app.listen(3001);
 }
+
 bootstrap();
