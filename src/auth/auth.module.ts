01cfd0c042380b4784033c707b5a9d703db22b9c
Maksym Tkachuk <maksym.tkachuk@infopulse.com>
2024-04-17 13:13:58 +0300

    update(*): move jwt.config to auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
-import { getJwtConfig } from 'src/config/jwt.config';
+import { getJwtConfig } from 'src/auth/config/jwt.config';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
