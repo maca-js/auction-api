357e436b4cff21dba7604eadf391971da91174cd
Maksym Tkachuk <maksym.tkachuk@infopulse.com>
2024-06-11 13:42:26 +0300

    add findByUserId

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  findByUserId(@Param('id') id: string) {
+    return this.userService.getById(id);
+  }
 }
